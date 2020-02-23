58d46cf6aa5fe036a84926c987a065da45bd504e
guochangyou <guochangyounihao@163.com>
2020-02-23 10:50:25 +0800

    add parse_model_config int utils/utils.py and add parse_model_config unit test in test/test.py

--- yolov3/pytorch/test/test.py
+++ yolov3/pytorch/test/test.py
@@ -1,12 +1,17 @@
+import sys
+sys.path.append("..")
 from utils.utils import *
 
 
 def test_parse_model_config(path):
     """test code for this function
     """
-    pass
+    networkStructure = parse_model_config(path)
+    print(len(networkStructure), networkStructure[0], networkStructure[1],
+          networkStructure[2], networkStructure[3])
     return
 
 
 if __name__ == "__main__":
-    pass
+    configFile = "../config/yolov3.cfg"
+    test_parse_model_config(configFile)
--- yolov3/pytorch/utils/utils.py
+++ yolov3/pytorch/utils/utils.py
@@ -3,10 +3,43 @@ def parse_model_config(path):
         1. 去除注释项.
         2. 去除两端空格.
         3. 返回一个列表. 其中每一块为一个词典，开头为type，后面是等号左右两侧对应key-value
-            注意卷积层的类中，有'batch_normaliz'项
+            注意卷积层的类中，有'batch_normalize'项
     """
     module_defs = []
 
-    pass
+    # “r+”读写操作
+    # f = open(path, "r+")
+    f = open(path, 'r')
+
+    # read([size]) 不指明size时，读取整个文件内容，注意内存问题
+    # contents = f.readlines()
+    contents = f.read().split('\n')
+    f.close()
+
+    # get rid of whitespaces
+    contents = [x for x in contents if x and not x.startswith('#')]
+    contents = [x.strip() for x in contents]
+
+    for line in contents:
+        # do this before loop
+        # line = line.strip()
+        # if line.startswith("#") or line == "":
+        #     continue
+        if line.startswith('['):
+            record = {}
+
+            # get rid of all possible whitespaces
+            record['type'] = line[1:-1].rstrip()
+            
+            # only convolutional cares about batch_normalize
+            if record['type'] == 'convolutional':
+                record['batch_normalize'] = 0
+            module_defs.append(record)
+        else:
+            # know split return two value
+            # line_part = line.split("=")
+            # module_defs[-1][line_part[0]] = line_part[1]
+            key, value = line.split("=")
+            module_defs[-1][key.rstrip()] = value.strip()
 
     return module_defs
